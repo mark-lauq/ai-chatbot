b62330551420717b94df7720eeeeb5e49f452998
Mark LauQ <mark.lauq@gmail.com>
2025-05-05 20:05:53 +0800

    revert(sidebar): remove fetch chats

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,60 +1,32 @@
 "use client";
 
-import { memo, useEffect, useState } from "react";
 import {
-  Sidebar,
-  SidebarHeader,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupLabel,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuItem,
-  SidebarMenuButton,
-} from "@/components/ui/sidebar";
-import { client } from "@/lib/api-client";
-import type { Chat } from "@/server/db/schema";
+  memo,
+  // useEffect, useState
+} from "react";
+import { Sidebar, SidebarHeader } from "@/components/ui/sidebar";
+// import { client } from "@/lib/api-client";
+// import type { Chat } from "@/server/db/schema";
 
 function PureAppSidebar() {
-  const [chats, setChats] = useState<Chat[]>();
+  // const [chats, setChats] = useState<Chat[]>();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await (await client.api.chat.$get()).json();
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const data = await (await client.api.chat.$get()).json();
 
-        setChats(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
+  //       setChats(data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <Sidebar className="border-r-gray-200">
-      <SidebarHeader className="font-bold">Chat History</SidebarHeader>
-      <SidebarContent>
-        {!chats ? (
-          <SidebarGroup>
-            <SidebarGroupLabel>Loading...</SidebarGroupLabel>
-          </SidebarGroup>
-        ) : (
-          <SidebarGroup>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {chats.map((chat) => (
-                  <SidebarMenuItem key={chat.title}>
-                    <SidebarMenuButton asChild>
-                      <a href={chat.id!}>{chat.title}</a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        )}
-      </SidebarContent>
+      <SidebarHeader className="font-bold">AI Chatbot</SidebarHeader>
     </Sidebar>
   );
 }
